3f2424c5bab16cd803a4f14b9141fb882b9f2425
« Hassina <hassinaayachi@hotmail.com>
2023-10-22 22:26:28 +0200

    :sparkles: set up crud

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
-    fetch("/books", {
+    fetch("/users/get", {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
@@ -7,81 +7,82 @@ window.addEventListener("DOMContentLoaded", function () {
 
     })
         .then((res) => res.json())
-        .then((books) => {
-            document.getElementById("bookList").innerHTML = books.map((book) => {
+        .then((users) => {
+            document.getElementById("userList").innerHTML = users.map((user) => {
                 return `
-                <li id="${book.id}">
-                    ID: ${book.id} <br> Name: ${book.name} <br> Author: ${book.author}
+                <li id="${user._id}">
+                        ID: ${user._id} <br> username: ${user.username} <br> email: ${user.email} <br> password: ${user.password} <br>
+                <button onclick="updateUser()" type="button" data-id="${user._id}">Patch User</button>
+                <button onclick="deleteUser()" type="button" data-id="${user._id}">Remove User</button>
                 </li>
             `
             }).join("");
         })
 }, false);
 
-const addNewBook = () => {
-    const newBook = prompt("Book name & author (separated by a comma)");
-    if (newBook) {
-        const [name, author] = newBook.split(",");
-        fetch("/books", {
+const addNewUser = () => {
+    const newUser = prompt("User username & email & password (separated by a comma)");
+    if (newUser) {
+        const [username, email, password] = newUser.split(",");
+        fetch("/users/post", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ name, author }),
+            body: JSON.stringify({ username, email, password }),
         })
             .then((res) => res.json())
             .then((res) => {
                 if (res.success) {
-                    document.getElementById("bookList").innerHTML += `
-                    <li id="${res.id}">
-                        ID: ${res.id} Name: ${name} <br> Author: ${author}
-                    </li>
-                `
+                    document.getElementById("userList").innerHTML += `
+                        <li id="${res._id}">
+                            ID: ${res._id} <br> username: ${username} <br> email: ${email} <br> password: ${password}
+                        </li>
+                    `
                 }
+                window.location.reload();
             });
     }
 };
 
-const deleteBook = () => {
-    const bookPrompt = prompt("Book ID");
-    if (!bookPrompt) return alert("Invalid book ID");
-    const bookId = parseInt(bookPrompt);
-    if (bookId) {
-        fetch(`/books/${bookId}`, {
+const updateUser = () => {
+    const userId = event.target.dataset.id;
+    if (userId) {
+        const newUser = prompt("User name & author (separated by a comma)");
+        if (newUser) {
+            const [username, email, password] = newUser.split(",");
+            fetch(`/users/patch/${userId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, email, password }),
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    if (res.success) {
+                        document.getElementById(userId).innerHTML = `
+                        ID: ${_id} <br> username: ${username} <br> email: ${email} <br> password: ${password}
+                        `;
+                    }
+                    window.location.reload();
+                });
+        }
+    }
+};
+
+const deleteUser = () => {
+    const userId = event.target.dataset.id
+    if (userId) {
+        fetch(`/users/delete/${userId}`, {
             method: "DELETE",
         })
             .then((res) => res.json())
             .then((res) => {
                 if (res.success) {
-                    document.getElementById(bookId).remove();
+                    document.getElementById(userId).remove();
                 }
+                window.location.reload();
             });
-    }
-};
-
-const updateBook = () => {
-    const bookPrompt = prompt("Book ID");
-    if (!bookPrompt) return alert("Invalid book ID");
-    const bookId = parseInt(bookPrompt);
-    if (bookId) {
-        const newBook = prompt("Book name & author (separated by a comma)");
-        if (newBook) {
-            const [name, author] = newBook.split(",");
-            fetch(`/books/${bookId}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name, author }),
-            })
-                .then((res) => res.json())
-                .then((res) => {
-                    if (res.success) {
-                        document.getElementById(bookId).innerHTML = `
-                        ID: ${bookId} <br> Name: ${name} <br> Author: ${author}
-                    `
-                    }
-                });
-        }
     }
 };
