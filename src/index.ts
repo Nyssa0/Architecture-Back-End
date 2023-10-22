069d8bf41dac95620c4eeb1cadb3ee50fd8b22b3
« Hassina <hassinaayachi@hotmail.com>
2023-10-22 22:26:03 +0200

    :sparkles: set up index

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,54 @@
-// import { Elysia } from "elysia";
-//
-// const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-//
-// console.log(
-//   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-// );
-
 import { Elysia, t } from 'elysia';
 import { html } from '@elysiajs/html'
-import { BooksDatabase } from './db.js';
+import { usersController } from "./controllers/users.controller";
+import './database/db.setup.ts'
+import { swagger } from '@elysiajs/swagger'
+import { helmet } from "elysia-helmet";
+import { basicAuth } from '@eelkevdbos/elysia-basic-auth'
 
-new Elysia()
-    .use(html())
-    .decorate("db", new BooksDatabase())
-    .get("/", () => Bun.file("src/index.html").text())
-    .get("/script.js", () => Bun.file("src/script.js").text())
-    .get("/books", ({ db }) => db.getBooks())
-    .post(
-        "/books",
-        async ({ db, body }) => {
-            const id = (await db.addBook(body)).id
-            return { success: true, id };
+process.env["BASIC_AUTH_CREDENTIALS"] = process.env.BASIC_AUTH_CREDENTIALS
+
+const app = new Elysia();
+
+// AUTHENTICATION
+app.use(basicAuth())
+    // all routes are protected by default
+app.get("/", () => "private")
+    // access to realm within a handler
+app.get('/private/realm-stored', ({ store }) => store.basicAuthRealm)
+
+// HTML
+app.use(html());
+
+// SWAGGER
+app.use(swagger({
+    path: '/swagger',
+    documentation: {
+        info: {
+            title: 'Bun.js CRUD app with Elysia.js',
+            version: '1.0.0',
         },
-        {
-            schema: {
-                body: t.Object({
-                    name: t.String(),
-                    author: t.String(),
-                }),
-            },
-        }
-    )
-    .put(
-        "/books/:id",
-        ({ db, params, body }) => {
-            try {
-                db.updateBook(parseInt(params.id), body)
-                return { success: true };
-            } catch (e) {
-                return { success: false };
-            }
-        },
-        {
-            schema: {
-                body: t.Object({
-                    name: t.String(),
-                    author: t.String(),
-                }),
-            },
-        }
-    )
-    .delete("/books/:id", ({ db, params }) => {
-        try {
-            db.deleteBook(parseInt(params.id))
-            return { success: true };
-        } catch (e) {
-            return { success: false };
-        }
-    })
-    .listen(3000);
+        tags: [
+            { name: 'PUBLIC', description: 'Public elements' },
+            { name: 'CRUD', description: 'CRUD elements' },
+        ]
+    }
+}))
+
+// INDEX ROUTE
+app.get("/", () => Bun.file("src/index.html"), {
+    detail: {
+        summary: 'Get index page',
+        tags: ['PUBLIC']
+    }
+})
+
+app.get("/script.js", () => Bun.file("src/script.js"))
+
+app.use(usersController)
+
+app.use(helmet({
+    contentSecurityPolicy: false,
+    XContentTypeOptions: false,
+    StrictTransportSecurity: false
+})).listen(3000);
